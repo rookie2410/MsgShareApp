9be319a7a46f0a66e92c3ca96d4a27895cef4ec1
Roshan Gupta <43931122+rookie2410@users.noreply.github.com>
2020-03-15 18:35:49 +0530

    Added Button

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.msgshareapp
 
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was tapped by user")
+
+            Toast.makeText(this ,"Button was tapped by user",Toast.LENGTH_SHORT).show()
+        }
     }
 }
